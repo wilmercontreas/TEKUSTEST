f8dd7b4778228737eef280cbc8d86d484d57af92
wilmer <wilmer.152.contreras@gmail.com>
2022-09-14 12:53:39 -0500

    project structure created and lazyload applied

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard', 
+    loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule),
+    // canActivate: [ ValidarTokenGuard ],
+    // canLoad : [ ValidarTokenGuard ]
+  },
+  { 
+    path: '**', 
+    redirectTo: 'auth' 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
